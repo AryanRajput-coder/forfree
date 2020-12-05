aff2666d0e0541ee28825d3d157715c591b3ad84
AryanRajput-coder <75056814+AryanRajput-coder@users.noreply.github.com>
2020-12-05 10:17:16 +0530

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fc9becae563f3001ecb6001",
+				"5fc9becae563f3001ecb6003"
+			]
+		},
+		"5fc9becae563f3001ecb6001": {
+			"id": "5fc9becae563f3001ecb6001",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fc9becae563f3001ecb6003": {
+			"id": "5fc9becae563f3001ecb6003",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app"
+}
